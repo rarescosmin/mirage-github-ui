a683256903d874125588d28533f9a863496bd04b
rarescosmin <rarescosmin97@gmail.com>
2021-11-10 13:32:19 +0200

    Deleted token

--- services/apollo-clients/githubApolloClient.ts
+++ services/apollo-clients/githubApolloClient.ts
@@ -7,7 +7,7 @@ const httpLink = createHttpLink({
 
 const authLink = setContext((_, { headers }) => {
     // get the authentication token from local storage if it exists
-    const token = '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';
+    const token = '';
     // return the headers to the context so httpLink can read them
     return {
       headers: {
